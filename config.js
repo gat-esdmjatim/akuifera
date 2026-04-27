0f720ece17d7cd3fb70489aa2cf05b5f4f081e32
gat-esdmjatim <geologiairtanahesdm@gmail.com>
2026-04-27 17:34:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  *
  * Contoh: 'https://script.google.com/macros/s/AKfycbxXXXXXXXXXXXXXXXXXXXXXX/exec'
  */
-var APPS_SCRIPT_URL = 'PASTE_URL_/exec_DARI_APPS_SCRIPT_DI_SINI';
+var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyofuWk7mMkHWjFZTAMsgHmawoHE2wG5Wc2Bsl3WyuVsid80QMM7uxnu_Z2lCIXUaXVIQ/exec';
 
 /**
  * Path dasar dari halaman GitHub Pages.
